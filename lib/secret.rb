e67db3421b70430fe2af21ec3bd2f5fc572a8997
crespire <li.simmon@gmail.com>
2021-11-07 14:45:04 -0500

    Add make_secret_word to Secret

--- lib/secret.rb
+++ lib/secret.rb
@@ -11,6 +11,14 @@ module Hangman
     end
 
     def make_secret_word
+      options = IO.readlines('5desk.txt', chomp: true)
+      selection = options.sample
+
+      until selection.length.between(5, 12) #hard code value for now, need to get this from options in the future
+        selection = options.sample
+      end
+
+      selection
     end
   end
 end
