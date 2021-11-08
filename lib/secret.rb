596b9b6fe3ee0f4a710a77b718f6cd075bda30cf
crespire <li.simmon@gmail.com>
2021-11-08 11:18:18 -0500

    Add guard clause for file existing

--- lib/secret.rb
+++ lib/secret.rb
@@ -19,6 +19,8 @@ module Hangman
     end
 
     def grab_secret_word
+      return nil unless File.exists?('5desk.txt')
+      
       @secret = IO.readlines('5desk.txt', chomp: true).filter { |w| w.length.between?(5, 12) }.sample.downcase
     end
 
