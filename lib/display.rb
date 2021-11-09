72ccd9435c78c83fa70cb272d30b41c2a1a7b769
crespire <li.simmon@gmail.com>
2021-11-09 17:12:05 -0500

    Add helper methods for Display.render

--- lib/display.rb
+++ lib/display.rb
@@ -5,5 +5,17 @@ module Hangman
   class Display
     def initialize
     end
+
+    def render(secret: secret, guesses: guesses, results: results)
+      #Do stuff with these values. Remember they can empty!
+    end
+
+    private
+    
+    def show_hangman(secret: secret, guesses: guesses, results: results)
+    end
+
+    def show_hits(secret: secret, guesses: guesses, results: results)
+    end
   end  
 end
